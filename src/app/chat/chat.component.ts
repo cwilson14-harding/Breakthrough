fd5e1419c875273be369c8be00c769c751795053
bradleyGamiMarques <bradleygamimarques@gmail.com>
2018-02-14 20:47:44 -0600

    Show/hide chat button
    Need help with styling, but the button works.

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -4,22 +4,37 @@ import {Observable} from "rxjs/Observable";
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
+  styleUrls: ['./chat.component.scss'],
+
 })
 export class ChatComponent implements OnInit {
   messagesCollection: AngularFirestoreCollection<any[]>;
   messages: Observable<any[]>;
-  constructor(public afs: AngularFirestore) { }
+  showStyle:false;
+
+  constructor(public afs: AngularFirestore) {
+  }
 
   ngOnInit() {
     this.getChatData();
   }
-getChatData(){
+
+  getChatData() {
     this.messagesCollection = this.afs.collection<any>('games');
-  this.messages = this.messagesCollection.valueChanges();
-}
+    this.messages = this.messagesCollection.valueChanges();
+  }
+
   newMessage(message) {
     this.messagesCollection.add(message);
   }
 
+  showHideChat() {
+    let div = document.getElementById("chatContainer");
+    if(div.style.display !== "none"){
+      div.style.display = "none";
+    }
+    else{
+      div.style.display ="block";
+    }
+  }
 }
