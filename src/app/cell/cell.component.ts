7fd27e83a2df8be1192fc8cce1cc4947189685d8
Curtis Wilson <cj@wilsontech.com>
2018-02-15 18:31:09 -0600

    Legend is now displayed inside each cell

--- src/app/cell/cell.component.ts
+++ src/app/cell/cell.component.ts
@@ -12,20 +12,19 @@ export class CellComponent implements OnInit {
   boardClass: string;
   @Input()
   legend: string;
-  get row(): number{
-    return +this.legend[0]+ 1;
+  get row(): number {
+    return +this.legend[0] + 1;
   }
-  get column(): string{
-    switch(this.legend[1])
-    {
-      case "0": return "A";
-      case "1": return "B";
-      case "2": return "C";
-      case "3": return "D";
-      case "4": return "E";
-      case "5": return "F";
-      case "6": return "G";
-      case "7": return "H";
+  get column(): string {
+    switch (this.legend[1]) {
+      case '0': return 'A';
+      case '1': return 'B';
+      case '2': return 'C';
+      case '3': return 'D';
+      case '4': return 'E';
+      case '5': return 'F';
+      case '6': return 'G';
+      case '7': return 'H';
     }
   }
   constructor() { }
