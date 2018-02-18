1818628a8dff6079113d62c5d921b05691ee28d8
dylangonzo <35376052+dylangonzo@users.noreply.github.com>
2018-02-18 17:31:11 -0600

    spelling
    corrected you're with your in the tutorial

--- src/app/tutorial/tutorial.component.ts
+++ src/app/tutorial/tutorial.component.ts
@@ -36,11 +36,11 @@ export class TutorialComponent implements OnInit {
         100);
       this.textAnimator = new TextAnimator();
       this.textAnimator.start(document.getElementById('messenger4'),
-        'The main objective of the game is to get one of you\'re pieces into the opponent\'s home row.',
+        'The main objective of the game is to get one of your pieces into the opponent\'s home row.',
         100);
       this.textAnimator = new TextAnimator();
       this.textAnimator.start(document.getElementById('messenger5'),
-        'The only other way to win is to capture all of you\'re opponent\'s pieces.',
+        'The only other way to win is to capture all of your opponent\'s pieces.',
         100);
   }
 }
