a145c4a991196302c5b3ed81b52ca3e5344dce78
Luke <luebelein@harding.edu>
2018-02-18 21:14:04 -0600

    lobby

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { AuthService } from '../core/auth.service';
-=======
+
 import {AuthService} from '../core/auth.service';
->>>>>>> 75fcdd1c7eae6490fce9c309c34bb68cf6e3679a
+
 
 @Component({
   selector: 'app-leaderboard',
