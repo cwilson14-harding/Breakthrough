082642a5fa35ce9d8b70bd476a7dc953b128655a
Curtis Wilson <cj@wilsontech.com>
2018-03-17 15:24:23 -0500

    Fixed singleplayer messaging.

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -28,58 +28,67 @@ export class ChatComponent implements OnInit {
   }
 
   ngOnInit() {
-    // Get the messages list for this game.
-    this.messagesCollection = this.db.collection<any>('chat-rooms').doc(this.gameService.gameId).collection('messages');
+    // Check if this is a networked game.
+    if (this.gameService.gameId) {
+      // Get the messages list for this game.
+      this.messagesCollection = this.db.collection<any>('chat-rooms').doc(this.gameService.gameId).collection('messages');
 
-    // Subscribe to the messages list.
-    this.messagesCollection.valueChanges().subscribe(messages => {
-      // Clear the display list of messages.
-      this.messages = [];
+      // Subscribe to the messages list.
+      this.messagesCollection.valueChanges().subscribe(messages => {
+        // Clear the display list of messages.
+        this.messages = [];
 
-      // Parse through the new list of messages and add each one to the display list.
-      for (const data of messages) {
-        const message: IMessage = {
-          message: data['message'],
-          senderName: data['senderName'],
-          senderId: data['senderId'],
-          time: new Date(data['time'])
-        };
+        // Parse through the new list of messages and add each one to the display list.
+        for (const data of messages) {
+          const message: IMessage = {
+            message: data['message'],
+            senderName: data['senderName'],
+            senderId: data['senderId'],
+            time: new Date(data['time'])
+          };
 
-        // Add this message to the list of messages.
-        this.messages.push(message);
-      }
-
-      // Sort the list of messages by time sent.
-      this.messages.sort((a, b) => {
-        if (a.time > b.time) {
-          return 1;
-        } else if (a.time < b.time) {
-          return -1;
-        } else {
-          return 0;
+          // Add this message to the list of messages.
+          this.messages.push(message);
         }
+
+        // Sort the list of messages by time sent.
+        this.messages.sort((a, b) => {
+          if (a.time > b.time) {
+            return 1;
+          } else if (a.time < b.time) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
       });
-    });
+    }
   }
 
   sendMessage(message) {
-    this.messagesCollection.add(message);
+    if (this.gameService.gameId) {
+      this.messagesCollection.add(message);
+    } else {
+      // Send offline message.
+      this.messages.push(message);
+    }
+  }
+
+  private makeMessage(message: string): IMessage {
+    return {
+      message: message,
+      senderName: this.currentUserName,
+      senderId: this.auth.getCurrentUser(),
+      time: Date.now()
+    };
   }
 
   newMessage() {
     // Get what is inside of the message box. This value will be stored inside of the message property inside of the Message interface.
     const messageBox: HTMLInputElement = document.getElementById('messageBox') as HTMLInputElement;
 
-    // Get the time when the message was sent.
-    const time = Date.now();
-    console.log(this.auth.getCurrentUser());
     // Create a message object.
-    const message: IMessage = {
-      message: messageBox.value,
-      senderName: this.currentUserName,
-      senderId: this.auth.getCurrentUser(),
-      time: time
-    };
+    const message: IMessage = this.makeMessage(messageBox.value);
 
     // Send the message.
     this.sendMessage(message);
